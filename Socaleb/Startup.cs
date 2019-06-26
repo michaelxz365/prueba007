ece8ddac7b8b8332bc85e80cf78595466bd1e38e
michaelxz365 <45070131+michaelxz365@users.noreply.github.com>
2019-06-26 09:26:17 -0400

    Agregar archivos de proyecto.

---
+++ Socaleb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Socaleb.Startup))]
+namespace Socaleb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
